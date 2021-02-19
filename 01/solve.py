0e00460ad5a4f49c083d51479d5e7c10cfabff7a
vonas <24623262+vonas@users.noreply.github.com>
2021-02-20 00:54:33 +0100

    Solve day 1 part 2

--- 01/solve.py
+++ 01/solve.py
@@ -8,6 +8,14 @@ data = [int(l.strip()) for l in stdin.readlines()]
 def calculate_fuel(mass):
   return int(mass / 3) - 2
 
-result = sum(calculate_fuel(value) for value in data)
-print(result)
+def calculate_fuel_total(mass):
+  last = calculate_fuel(mass)
+  total = last
+  while last > 0:
+    last = max(0, calculate_fuel(last))
+    total += last
+  return total
+
+print(sum(calculate_fuel(value) for value in data))
+print(sum(calculate_fuel_total(value) for value in data))
 
